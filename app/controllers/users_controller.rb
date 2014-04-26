bd67b9169c495837d771985748e81342334ab8fc
davidmiller11 <davidmiller11@gmail.com>
2014-04-26 12:25:10 -0400

    Add edit, update and destroy actions to UsersController. Remove create, destroy and update views from Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      redirect_to users_path
     else
       render 'new'
     end
@@ -26,9 +26,18 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update!(user_params)
+      redirect_to user
+    else
+      render user
+    end
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy!
+    redirect_to users_path
   end
 
   private
