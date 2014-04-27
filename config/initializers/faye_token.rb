8d5ab2eac473bd1e25335d3dfe337136856aee61
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-27 12:46:17 -0400

    updated the environmental variables

--- config/initializers/faye_token.rb
+++ config/initializers/faye_token.rb
@@ -1,2 +1,3 @@
-ENV['faye_server'] = "http://nameless-brook-7007.herokuapp.com/faye"
+ENV['FAYE_SERVER'] = "http://nameless-brook-7007.herokuapp.com/faye"
+ENV['FAYE_JS_FILE'] = "http://nameless-brook-7007.herokuapp.com/faye.js"
 FAYE_TOKEN = "super_toe_jam"
