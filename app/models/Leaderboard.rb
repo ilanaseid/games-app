ca634575e2f903ca854e140dbbd13ba1d7ed234e
urbantumbleweeed <saunders.stephen@gmail.com>
2014-04-27 21:54:41 -0400

    added leaderboard controller and model.  added actions to user_challenges

---
+++ app/models/Leaderboard.rb
@@ -0,0 +1,6 @@
+class Leaderboard 
+
+	def self.leader(users)
+		//find the user id with the most 
+	end
+end
--- app/models/user_challenge.rb
+++ app/models/user_challenge.rb
@@ -1,3 +1,11 @@
 class UserChallenge < ActiveRecord::Base
 validates :user_id, :challenge_id, :win, presence: true
+
+	def self.leader
+		self.winners.select("user_id as player, count(win) as wins").group("count(win) desc")
+	end
+
+	def self.winners
+		return self.where("win = ?", true).distinct
+	end
 end
